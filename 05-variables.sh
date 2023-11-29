fe12f24d89f3207f11fa9bf8f98e9c25403e9875
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 09:54:23 +1100

    Username and Passwd

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Please enter your username"
+
+read USERNAME # This is the variable where username is stored
+
+echo "Username entered is $USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD # This is the variable where username is stored
+
+echo "Username entered is $PASSWORD"
