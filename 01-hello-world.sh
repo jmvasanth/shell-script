53cdd0a8adc510f5500cce46dd575b4544f155db
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 09:33:08 +1100

    Ramesh and Suresh convo

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
 echo "This is my first shell script"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are doing Suresh"
+echo "Suresh: I am doing good Ramesh"
