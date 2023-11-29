410cc0ed95f42f3fcfd5394be041f959cff89d19
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 09:46:18 +1100

    Input from CLI

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are doing $PERSON2"
+echo "$PERSON2: I am doing good $PERSON1"
