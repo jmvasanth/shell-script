eca846bd3c10e44d519cfe7c9a904874a9af09a4
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 13:22:17 +1100

    loops enhanced

---
+++ 12-loops.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#Install all the packages received as an input 
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+USERID=$(id -u)
+if [$USERD -ne 0]
+then
+    echo "Please run the script as root user"
+    exit 1
+fi
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILURE $N"
+    else
+        echo -e "$2 .... $G SUCCESS $N"
+    fi
+}
+
+for i in $@
+do
+    yum install $i -y &>>$LOGFILE
+    VALIDATE $? "Installing $i"
+done
