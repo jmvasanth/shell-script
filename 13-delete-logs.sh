141d22f9ab029dcbd87571abddd9e51a09534450
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 16:39:41 +1100

    loops enhanced

---
+++ 13-delete-logs.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F:%H:%M:%S)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "Script started executing at $DATE" &>>$LOGFILE
+
+while read line
+do
+    echo "Deleting $line" &>>$LOGFILE
+    rm -rf $line
+done <<< $FILES_TO_DELETE
+
