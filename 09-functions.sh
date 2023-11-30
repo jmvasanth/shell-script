dd45f7871f0eddfb749a4391558989b1246ce517
jmvasanth <jmvasanth@protonmail.com>
2023-11-30 12:13:51 +1100

    colors

--- 09-functions.sh
+++ 09-functions.sh
@@ -5,13 +5,17 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 #This functions will validate the previous command
 VALIDATE (){
     if [ $1 -ne 0 ]
     then
-        echo "$2 .... FAILURE"
+        echo -e "$2 .... $R FAILURE $N"
     else
-        echo "$2 .... SUCCESS"
+        echo "$2 .... $G SUCCESS $N"
     fi
 }
 USERID=$(id -u)
